3659c43ff993826f74b9b619310dbaaf2adae44c
JimmyKao <jimmy.kao.jea@gmail.com>
2023-06-30 12:06:16 +0800

    first commit

---
+++ api/finance.py
@@ -0,0 +1,180 @@
+import numpy as np
+import pandas as pd  
+import yfinance as yf
+import matplotlib.pyplot as plt
+import datetime
+import pyimgur
+# from imgurpython import ImgurClient
+
+class Finance:
+    def __init__(self):
+        self.symbol = {}
+
+    def getDate(self):
+        DateTime = datetime.datetime.now()
+        Y1 = DateTime.year
+        M1 = DateTime.month
+        D1 = DateTime.day
+        dateStr = str(Y1)+"/"+str(M1)+"/"+str(D1)    
+        return dateStr
+    
+    def getData(self, symbol):
+        # 下載股價資料-最近1年
+        df = yf.download(symbol, period='1y')
+        
+        # 計算移動平均線
+        df['MA20'] = df['Close'].rolling(window=20).mean()
+        df['MA60'] = df['Close'].rolling(window=60).mean()
+
+        # 從資料中提取最高價（high）和最低價（low） 
+        high_values = df['High'].values 
+        low_values = df['Low'].values
+
+        # 計算 william 指數
+        window_size = 20
+        high_values = df['High'].rolling(window_size).max()
+        low_values = df['Low'].rolling(window_size).min()
+        df['WILLIAMS'] = ((high_values - df['Close'][-1]) / (high_values - low_values)) * 100
+
+        # 計算 MFI 資金流向指標
+        adl = ((df['Close'] - df['Low']) - (df['High'] - df['Close'])) / (df['High'] - df['Low']) * df['Volume']
+        adl = adl.cumsum()
+        df['ADL'] = adl
+
+        # 計算 MFI
+        typical_price = (df['High'] + df['Low'] + df['Close']) / 3
+        money_flow = typical_price * df['Volume']
+        positive_flow = np.where(typical_price > typical_price.shift(1), money_flow, 0)
+        negative_flow = np.where(typical_price < typical_price.shift(1), money_flow, 0)
+        positive_flow_sum = pd.Series(positive_flow).rolling(window=14).sum()
+        negative_flow_sum = pd.Series(negative_flow).rolling(window=14).sum()
+        money_flow_ratio = np.where(negative_flow_sum == 0, 0.5, positive_flow_sum / negative_flow_sum)
+        mfi = 100 - (100 / (1 + money_flow_ratio))
+        df['MFI'] = mfi
+
+        # 計算RSI指標
+        delta = df['Close'].diff()
+        gain = delta.where(delta > 0, 0)
+        loss = -delta.where(delta < 0, 0)
+        avg_gain = gain.rolling(window=14).mean()
+        avg_loss = loss.rolling(window=14).mean()
+        rs = avg_gain / avg_loss
+        df['RSI'] = 100 - (100 / (1 + rs))
+
+        # 計算布林通道
+        df['std'] = df['Close'].rolling(window=20).std()
+        df['upper'] = df['MA20'] + 2 * df['std']
+        df['lower'] = df['MA20'] - 2 * df['std']
+
+        # 利用前一個月的布林通道上下限來計算最佳買入價格和停損價格
+        df['diff'] = df['upper'] - df['lower']
+        df['buy_price'] = df['lower'].rolling(window=45).mean() + 0.2 * df['diff'].rolling(window=45).mean()
+    
+        # 這個公式的目的是要讓投資人在股價處於低點時買進，
+        # 並且設定一個停損價格，以控制風險。
+        # 這個公式的核心是利用布林通道的下限來判斷股價是否處於低點，
+        # 再加上一個緩衝區間，以避免買進時股價已經反彈。
+        # 而這個緩衝區間的大小，就是由0.2倍的布林通道上下限區間差來決定的。
+        df['stop_loss_price'] = df['lower'].rolling(window=45).mean() + 0.1 * df['diff'].rolling(window=45).mean()
+
+        # 找出最佳買入點
+        df['signal'] = 0
+        df.loc[(df['Close'] < df['lower'] * 1.05) & (df['RSI'] < 30) & (df['Close'] <= df['buy_price']), 'signal'] = 1
+
+        return df
+    
+    def upload(self):
+        client_id = 'cbac1b726bffaa2'
+        # client_secret = 'aa6359f77337408decf5e590c70a13b6fcba346b'
+        # client = ImgurClient(client_id, client_secret)
+
+        im = pyimgur.Imgur(client_id)
+        path = "img.png"
+        uploaded_image = im.upload_image(path, title='Uploaded with PyImgur')
+        # print(uploaded_image.link)
+        return uploaded_image.link
+
+
+
+    def getImg(self, symbol):
+        df = self.getData(symbol)
+        
+        # 繪製趨勢圖
+        fig, ax = plt.subplots(figsize=(16, 9))
+
+        ax.plot(df.index, df['Close'], lw=3,marker='.',color='#FF0000', label='C_Price')
+        ax.plot(df.index, df['MA20'], lw=3,color='#EE7700', linestyle='-', label='MA20 day')
+        ax.plot(df.index, df['MA60'], lw=3,color='#7700BB', linestyle='-', label='MA60 day')
+        ax.plot(df.index, df['upper'], color='k', linestyle='--', label='Bollinger Bands_Upper')
+        ax.plot(df.index, df['lower'], color='k', linestyle='--', label='Bollinger Bands_Lower')
+
+        ax.fill_between(df.index, df['upper'], df['lower'], alpha=0.2)
+
+        # 顯示最近一天收盤價的RSI值
+        ax2 = ax.twinx()
+        ax2.plot(df.index, df['RSI'], lw=1,color='b', label='RSI')
+        ax2.axhline(y=30, lw=1,color='g', linestyle='--')
+        ax2.axhline(y=70, lw=1,color='g', linestyle='--')
+        ax2.set_ylim([0, 100])
+        ax2.set_ylabel('RSI')
+        ax2.legend(loc='upper left')
+
+        ax.legend()
+        ax.set_title(symbol + '~' + self.getDate())
+        ax.set_xlabel('Date')
+        ax.set_ylabel('Price')
+
+        fig.savefig("img.png")
+
+        return self.upload()
+    
+    def getReplyMsg(self, symbol, name):
+        df = self.getData(symbol)
+
+        last_close = df['Close'][-1]
+        last_buy_price = df['buy_price'][-1]
+        last_stop_loss_price = df['stop_loss_price'][-1]
+        last_rsi = df['RSI'][-1]
+
+        if df['signal'][-1] == 1:
+            keyword = symbol + f'最近一天收盤價[{last_close:.2f}元], 已經出現買入訊號了!\n' \
+                    + '**買入訊號設定條件為: 股價已低於布林通道下軌線的1.05倍、且RSI指標小於30、且股價低於估算的合理買入價格。\n' \
+                    + '建議趕快買進，買入參考價格為' + str(round(last_buy_price*0.995, 2)) + '元。(祝發大財~)\n' \
+                    + '另提供參考停損價格為' + str(round(last_stop_loss_price, 2)) + '元。'    
+        elif last_close >= df['MA20'][-1] and last_close >= df['MA60'][-1] and last_close >= df['upper'][-1]*0.995:
+            keyword = symbol + f'最近一天收盤價[{last_close:.2f}元], 已高於MA20月均線及MA60季均線且超過布林上限。\n' \
+                    + '強烈建議可賣出。\n' \
+                    + '賣出參考價格:' + str(round(last_close*1.005, 2)) + '元。(發大財了~)\n' \
+                    + 'RSI指標為' + str(round(last_rsi, 2))   
+        elif last_close >= df['MA20'][-1] and last_close >= df['upper'][-1]*0.995:
+            keyword = symbol + f'最近一天收盤價[{last_close:.2f}元], 已高於MA20月均線且超過布林上限。\n' \
+                    + '若有賣出計畫，建議可賣出。\n' \
+                    + '賣出參考價格為' + str(round(last_close*1.005, 2)) + '元。\n' \
+                    + 'RSI指標為' + str(round(last_rsi, 2)) 
+        elif last_close <= df['MA20'][-1] and last_close <= df['MA60'][-1] and last_close <= df['lower'][-1]*1.005:
+            keyword = symbol + f'最近一天收盤價[{last_close:.2f}元], 已低於MA20月均線及MA60季均線且低於布林下限。\n' \
+                    +'強烈建議可加碼買進!!! (讓您發財~)\n' \
+                    +'RSI指標為' + str(round(last_rsi, 2))    
+        elif last_close <= df['MA20'][-1] and last_close <= df['lower'][-1]*1.005:
+            keyword = symbol + f'最近一天收盤價[{last_close:.2f}元], 已低於MA20月均線且低於布林下限。\n' \
+                    + '若有要加碼買進的計畫，建議可以準備了喔。 (讓您發財~)\n' \
+                    + 'RSI指標為' + str(round(last_rsi, 2))    
+        else:
+            keyword = symbol + f'最近一天收盤價[{last_close:.2f}元], 還在布林通道內。\n' \
+                    + '建議持續觀望~\n' \
+                    + 'RSI指標為' + str(round(last_rsi, 2))
+            
+        replyMsg = self.getDate() + "\n"
+        replyMsg += symbol + ' ' + name + '\n'
+        replyMsg += "近45天交易數據的最佳買點: " + str(round(last_buy_price, 2)) + "元\n"
+        replyMsg += "*布林通道上限: " + str(round(df['upper'][-1],2)) + "\n"
+        replyMsg += "*月均線參考值: " + str(round(df['MA20'][-1],2)) + "\n"
+        replyMsg += "*季均線參考值: " + str(round(df['MA60'][-1],2)) + "\n"
+        replyMsg += "*布林通道下限: " + str(round(df['lower'][-1],2)) + "\n"
+        replyMsg += "*Williams[20]: " + str(round(df['WILLIAMS'][-1],2)) + "%\n"
+        replyMsg += "*MFI資金流向指標: " + str(round(df['MFI'][-1],2)) + "\n"
+        replyMsg += "*A/D Line指標: " + str(round(df['ADL'][-1]/10000,2)) + '\n\n'
+        replyMsg += '操作建議: \n'
+        replyMsg += keyword + "\n"
+
+        return replyMsg
---
+++ api/index.py
@@ -0,0 +1,115 @@
+from flask import Flask, request, abort
+from linebot import LineBotApi, WebhookHandler
+from linebot.exceptions import InvalidSignatureError
+from linebot.models import MessageEvent, TextMessage, TextSendMessage, ImageSendMessage
+from api.finance import Finance
+# import os
+
+# ETF, 績優股, 金融股
+TYPE = "1. ETF \n2. 績優股 \n3. 金融股"
+ETF = {
+    '00878.TW': '國泰永續高股息', 
+    '00919.TW': '群益台灣精選高息', 
+    '00713.TW': '元大台灣高息低波', 
+    '0056.TW': '元大高股息', 
+    '006208.TW': '富邦台灣50', 
+    '00690.TW': '兆豐藍籌30', 
+    '00850.TW': '元大臺灣ESG永續', 
+    '00701.TW': '國泰股利精選30'
+}
+BLUE_CHIP = {
+    '2356.TW': '英業達',
+    '2330.TW': '台積電', 
+    '2454.TW': '聯發科', 
+    '2357.TW': '華碩', 
+    '6505.TW': '台塑',
+    '1102.TW': '亞泥',
+    '2324.TW': '仁寶'
+}
+FINANCIAL = {
+    '2890.TW': '永豐金',
+    '2886.TW': '兆豐金',
+    '2885.TW': '元大金'
+}
+
+line_bot_api = LineBotApi('CnxTIV3ZENKBF4uLOFI2x2I2wwG7Y0ILmp0pR+TvHbE/pbTPpTxw3ea5qrfsfB/T4xnXZdwuBZHgFK+eXz/bE86B8Ge+YBtEt6mEduMjFf5Pi/VsNv5PrUkgK+AtTFKAKF1H05phg7v3dkKtDuSzYgdB04t89/1O/w1cDnyilFU=')
+line_handler = WebhookHandler('11ce307d39f4e16e81dc9c49c3353ca9')
+# line_bot_api = LineBotApi(os.getenv("LINE_CHANNEL_ACCESS_TOKEN"))
+# line_handler = WebhookHandler(os.getenv("LINE_CHANNEL_SECRET"))
+# working_status = os.getenv("DEFALUT_TALKING", default = "true").lower() == "true"
+
+app = Flask(__name__)
+finance = Finance()
+
+# domain root
+@app.route('/')
+def home():
+    return 'Hello, World!'
+
+@app.route("/webhook", methods=['POST'])
+def callback():
+    # get X-Line-Signature header value
+    signature = request.headers['X-Line-Signature']
+    # get request body as text
+    body = request.get_data(as_text=True)
+    app.logger.info("Request body: " + body)
+    # handle webhook body
+    try:
+        line_handler.handle(body, signature)
+    except InvalidSignatureError:
+        abort(400)
+    return 'OK'
+
+
+@line_handler.add(MessageEvent, message=TextMessage)
+def handle_message(event):
+    message = []
+    msg = event.message.text
+    if event.message.type != "text":
+        return
+    
+    if msg == "選股":
+        reply = TYPE
+        message.append(TextSendMessage(text = reply))
+    elif msg == "ETF" or msg == "etf" or msg == "1" or msg == "1.":
+        reply = "ETF: \n"
+        for key, val in ETF.items():
+            reply += key + " : " + val + "\n"
+        message.append(TextSendMessage(text = reply))
+    elif msg == "績優股" or msg == "2" or msg == "2.":
+        reply = "績優股: \n"
+        for key, val in BLUE_CHIP.items():
+            reply += key + " : " + val + "\n"
+        message.append(TextSendMessage(text = reply))
+    elif msg == "金融股" or msg == "3" or msg == "3.":
+        reply = "金融股: \n"
+        for key, val in FINANCIAL.items():
+            reply += key + " : " + val + "\n"
+        message.append(TextSendMessage(text = reply))
+    elif msg in ETF or msg in BLUE_CHIP or msg in FINANCIAL:
+        if msg in ETF:
+            reply = finance.getReplyMsg(msg, ETF[msg])
+            # img_url = finance.getImg(msg)
+        elif msg in BLUE_CHIP:
+            reply = finance.getReplyMsg(msg, BLUE_CHIP[msg])
+            # img_url = finance.getImg(msg)
+        elif msg in FINANCIAL:
+            reply = finance.getReplyMsg(msg, FINANCIAL[msg])
+            # img_url = finance.getImg(msg)
+        else:
+            reply = '抱歉，請再試一次'
+
+        message.append(TextSendMessage(text = reply))
+        # message.append(ImageSendMessage(
+        #     original_content_url = img_url,
+        #     preview_image_url = img_url
+        # ))
+    else:
+        reply = "我不知道你在說什麼"
+
+    line_bot_api.reply_message(event.reply_token, message)
+
+    return 
+
+if __name__ == "__main__":
+    app.run()
