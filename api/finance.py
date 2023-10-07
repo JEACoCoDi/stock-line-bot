0e437904a0a6e489cfaa1806cd30c073929d20f4
JimmyKao <jimmy.kao.jea@gmail.com>
2023-10-07 23:09:45 +0800

    修正部分參數估算方式；漲跌與比例

--- api/finance.py
+++ api/finance.py
@@ -113,6 +113,17 @@ class Finance:
         last_stop_loss_price = df['stop_loss_price'][-1]
         last_rsi = df['RSI'][-1]
 
+        #計算收盤漲跌
+        if df['Close'][-1] > df['Close'][-2] :
+            mark = "漲"
+            #prop = round(((df['Close'][-1] - df['Close'][-2])/df['Close'][-2]) * 100,2)
+        elif df['Close'][-1] == df['Close'][-2] :
+            mark = "平"
+        else:
+            mark = "跌" 
+        #計算收盤漲跌比例
+        prop = round(((df['Close'][-1] - df['Close'][-2])/df['Close'][-2]) * 100,2)
+
         #計算季均價差值
         price_difference = df['Close'][-1] - df['MA60'][-1]
 
@@ -184,7 +195,7 @@ class Finance:
         replyMsg += keyword + "\n\n"
         replyMsg += "===開盤參數===" + "\n"                
         replyMsg += "*最高價H: " + str(round(df['High'][-1], 2)) + "元\n"
-        replyMsg += "*收盤價C: " + str(round(df['Close'][-1], 2)) + "元\n"
+        replyMsg += "*收盤價C: " + str(round(df['Close'][-1], 2)) + "元 (" + mark + prop +"%)\n"
         replyMsg += "*最低價L: " + str(round(df['Low'][-1], 2)) + "元\n"
         replyMsg += "*成交量V: " + str(df['Volume'][-1]/1000) + "張\n"
         replyMsg += "*K(9): " + str(round(df['K'][-1], 2)) + "\n"
